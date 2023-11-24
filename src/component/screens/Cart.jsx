b3f7751c0588c3a9d5b95493a48617f9bd2e5a9b
Rohan <rohanb158@gmail.com>
2023-11-24 23:08:35 +0530

    added some cart code

--- src/component/screens/Cart.jsx
+++ src/component/screens/Cart.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import Cookies from 'js-cookie';
 import { useSelector , useDispatch } from 'react-redux';
+import Navbar from '../common/Navbar'
+import img from "../assets/samsumg.jpg"
+import './Cart.scss'
 function Cart() {
     const [phone_data,setphone_data]=useState([]);
 
@@ -13,29 +16,47 @@ function Cart() {
      const cart_data = [...set];
      console.log(cart_data)
   return (
-    <div>
+  <div>
+        <Navbar></Navbar><div className='cart-main'>
+        <div className='items'>
         {
              
-            phone_data.map(data =>{
-              const found = cart_data.find((elem)=> elem === data.id)
-             if(found){  return(
-                    <div className='product'>
-                      {data.p_name}<br>
-                      </br>
-                      {data.orignal_price}<br></br>
-                    </div>
-                )}
-              
-                    return;
-        
-            })
-          
+             phone_data.map(data =>{
+               const found = cart_data.find((elem)=> elem === data.id)
+              if(found){  return(
+                     <div className='product'>
+                      <CartCard data={data}></CartCard>
+                     </div>
+                 )}
+               
+                     return;
+         
+             })
+           
+         }
+         {
         }
-  {
-    
-  }
-    </div>
+        
+        </div>
+        <div className='checkout'></div>
+      </div>
+</div>
   )
+}
+function CartCard({data}){
+return(
+  <>
+  <div className='CartCard'>
+   
+      <img src={img} className='product-image'></img>
+
+    <div className='product-name'> {data.p_name} </div>
+    <div className='product-quantity'> 1</div>
+    <div className='product-price'>{data.orignal_price}</div>
+  </div>
+  </>
+)
+
 }
 
 export default Cart
